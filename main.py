898ba6107c94d5f064c1451fd23a571a4c888946
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-07-09 08:50:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 #a = cards in hands and feild
 #b = monster lv
 #x = xyz monster
-#y = fusion monster
-
-
+#y = fusion monster = monster lv
 
 a = input()
 b = input()
 
-a + b = ab
+#a = 2*x + y(=b)
+#(a - b) /2 = x
+#b = y
 
-#(ab - 1) / 2 =
+
+
 
 # xyz to banish = x
-print("xyz monster level :"+
-     )
+print("xyz monster level :"+x)
  
 
 
 # fusion to banish = y = b
 print("fusion monster level :"+b)
+print("if level"+b+"futuon monster is banished by pot, can use cannon now.)
