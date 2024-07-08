3832f537708d645864499b0aa13fd7e7aace8c4a
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-07-09 08:42:22 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#a = cards in hands and feild
+#b = monster lv
+#x = xyz monster
+#y = fusion monster
+
+
+
+a = input()
+b = input()
+
+a + b = ab
+
+#(ab - 1) / 2 = 
+
+
+# fusion to banish = b
+print("fusion monster level :"+b)
