1a52eb687b5f334412f534e14a95ce4617551df5
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-07-09 09:00:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,19 @@
 #b = monster lv
 #x = xyz monster
 #y = fusion monster = monster lv
-a = 0
-b = 0
-a = int(input())
-b = int(input())
+
+a = input()
+b = input()
 
 #a = 2*x + y(=b)
-#(a - b) /2 = x
-#b = y
+#x = (a - b) /2
+#y = b 
 
 x = (a - b) / 2
 
-
 # xyz to banish = x
-print("xyz monster level :"+str(x))
- 
-
+print("xyz monster level :"+x)
 
 # fusion to banish = y = b
-print("fusion monster level :"+str(b))
-print("if level"+str(b)+"futuon monster is banished by pot, can use cannon now.)
+print("fusion monster level :"+b)
+print("if level"+b+"futuon monster is banished by pot, can use cannon now.)
